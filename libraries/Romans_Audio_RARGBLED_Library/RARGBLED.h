b2637b00e69a8b10a9387d0b9c315f034ef2cc5b
crownnsoda <crownnsoda@gmail.com>
2019-01-08 09:28:49 -0600

    true up

---
+++ libraries/Romans_Audio_RARGBLED_Library/RARGBLED.h
@@ -0,0 +1,52 @@
+/*
+RARGBLED v1.0.0
+(c) 2017 by Michael Romans of Romans Audio
+
+Simple OOP version of a RGP LED with simple controls
+RARGBLED myRGB(12,11,10);
+myRGB.setRGBColor(128,0,255); Sets the RGBLED to it's RGB value.
+myRGB.twinkle(60); Twinkles the LED for 60 millis
+myRGB.setColor(RED); Makes the LED Red.
+myRGB.off(); Shuts it off.
+myRGB.on(); Turns it back on to the last color used.
+*/
+#ifndef RARGBLED_H
+#define RARGBLED_H
+#include <RAGPIO.h>
+
+enum COLOR {
+    RED,
+    GREEN,
+    BLUE,
+    WHITE,
+    YELLOW,
+    CYAN,
+    PURPLE,
+    MAGENTA
+};
+
+class RARGBLED {
+     public:
+          #define CommonAnode 1
+          #define CommonCathode 0
+          RARGBLED(int redPin, int greenPin, int bluePin, int config = CommonAnode);
+          DigitalOut M_REDPIN(redPin,0);
+          void setRGBColor(int red, int green, int blue);
+          void off();
+          void on();
+          void twinkle(unsigned long mil);
+          void setColor(COLOR color);
+          void setFadeBGR(int lowNumber, int highNumber, int value);
+          void setFadeRGB(int lowNumber, int highNumber, int value);
+          void police(int times,int delayTime);
+     private:
+          int m_RedPin;
+          int m_GreenPin;
+          int m_BluePin;
+          int m_Red;
+          int m_Green;
+          int m_Blue;
+          int m_Config;
+};
+
+#endif
